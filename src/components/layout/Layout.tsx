cf976264dd7469a8d8d7867eb0a4599ba6f6b6e9
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-12-10 14:39:22 -0500

    Added the class to the proper place.

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Auxillary from "../../hoc/Auxillary";
+import classes from './layout.module.css';
 
 const layout = (props: any) => (
     // This is for wrapping two adjacent elements.
     // Aux is a reserved keyword in Windows, so therefore I used
     // the full word.
     <Auxillary>
-        <div>
+        <div className={classes.Content}>
             Toolbar, SideDrawer, Backdrop
         </div>
         <main>
