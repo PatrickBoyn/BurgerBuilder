ab162ab956a6a7455832cfe8e2d5cb144972f9ed
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-12-13 15:22:00 -0500

    TypeScript complains if there's implicit styling.

--- src/components/Burger/Burger.tsx
+++ src/components/Burger/Burger.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import classes from './Burger.module.css'
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
-const Burger = (props: string) => {
+
+const Burger = (props: any) => {
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
