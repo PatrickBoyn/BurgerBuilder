8a07e8524cb01605faac38bca04fc2bc75043580
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-12-14 12:28:08 -0500

    Added a type, and added a reduce method to get the individual elements of the array.

--- src/components/Burger/Burger.tsx
+++ src/components/Burger/Burger.tsx
@@ -3,12 +3,18 @@ import classes from './Burger.module.css'
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const Burger = (props: any) => {
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients: any = Object.keys(props.ingredients)
         .map(ingredientKey => {
             return [...Array(props.ingredients[ingredientKey])].map((_, i) => {
                 return <BurgerIngredient key={ingredientKey + i} type={ingredientKey}/>
             })
-        });
+        })
+        .reduce((array, element) => {
+            return array.concat(element);
+        }, []);
+    if (transformedIngredients.length === 0){
+        transformedIngredients = <p>Please add an ingredient!</p>
+    }
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
