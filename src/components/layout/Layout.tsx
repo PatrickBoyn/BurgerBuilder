3d9f60dcca6800473c38468737a9663378ce0c22
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-12-14 12:28:53 -0500

    Fixed a typo.

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import Auxillary from "../../hoc/Auxillary";
+import Auxiliary from "../../hoc/Auxiliary";
 import classes from './layout.module.css';
 
 const layout = (props: any) => (
     // This is for wrapping two adjacent elements.
     // Aux is a reserved keyword in Windows, so therefore I used
     // the full word.
-    <Auxillary>
+    <Auxiliary>
         <div className={classes.Content}>
             Toolbar, SideDrawer, Backdrop
         </div>
         <main>
             {props.children}
         </main>
-    </Auxillary>
+    </Auxiliary>
 );
 
 export default layout;
